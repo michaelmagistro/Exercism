afee620aab56b86098d93fa1b76d28edf32194ec
Michael Magistro <singleton1184@gmail.com>
2019-09-02 09:06:01 -0600

    scrabble score

---
+++ python/scrabble-score/scrabble_score.py
@@ -0,0 +1,9 @@
+def score(word):
+    letters = ['AEIOULNRST', 'DG', 'BCMP', 'FHVWY', 'K', 'JX', 'QZ']
+    points = [1, 2, 3, 4, 5, 8, 10]
+    score_brackets = len(points)
+    score = 0
+    for i in word:
+        for j in range(score_brackets):
+             if i.lower() in letters[j].lower(): score += points[j]
+    return score
